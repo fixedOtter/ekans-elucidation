93efbaf93fdff8c4f352d886e2222c837726a1c9
fixedotter <fixedotter@debian-tiger.lan>
2026-03-15 14:14:32 -0700

    added logic for plotting arrays of different length

--- graphMongo.py
+++ graphMongo.py
@@ -53,160 +53,119 @@ async def main():
 
     # new and improved query
     queryssnr = {"ssnamenr": {"$in": realObjects}}
-    
-    currentObjects = realCol.find(queryssnr) # gets ssnamenr objects
-    gungunObjects = gunguntestCol.find(queryssnr) # runs query on gungun collection
 
-    # # initializing arrays
-    # gungunObjData1 = [[],[]]
-    # currentObjData1 = [[],[]]
-    # gungunObjData2 = [[],[]]
-    # currentObjData2 = [[],[]]
-    # gungunObjData3 = [[],[]]
-    # currentObjData3 = [[],[]]
+    # rewritten code so i don't exhaust the cursors
+    current_cursor = realCol.find(queryssnr)
+    gungun_cursor = gunguntestCol.find(queryssnr)
 
-    # better arrays
-    currentssnr = []
-    current1 = []
-    current2 = []
-    current3 = []
-    gungunssnr = []
-    gungun1 = []
-    gungun2 = []
-    gungun3 = []
+    # build a map of all the current data cursor
+    # doing this so i'm not double dipping on one cursor
+    current_data_map = {}
+    async for obj in current_cursor:
+        if "periods" in obj and isinstance(obj["periods"], dict):
+            try:
+                # daniel has these deriven into three different periods
+                current_data_map[obj["ssnamenr"]] = [
+                    obj["periods"]["periods"][1]["period"],
+                    obj["periods"]["periods"][3]["period"],
+                    obj["periods"]["periods"][5]["period"]
+                ]
+            except (IndexError, KeyError):
+                continue
 
-    # saves object data for current
-    async for object in currentObjects:
-      ssnr = object["ssnamenr"]
-      per1 = object["periods"]["periods"][1]["period"]
-      per2 = object["periods"]["periods"][3]["period"]
-      per3 = object["periods"]["periods"][5]["period"]
+    # initialize the arrays
+    # we'll append to these as we loop through, and verify they exist in both queries.
+    plot_ssnr = []
+    plot_g1, plot_c1 = [], []
+    plot_g2, plot_c2 = [], []
+    plot_g3, plot_c3 = [], []
 
-      currentssnr.append(ssnr)
-      current1.append(per1)
-      current2.append(per2)
-      current3.append(per3)
+    async for obj in gungun_cursor:
+        ssnr = obj["ssnamenr"]
+        # only append if in both
+        if ssnr in current_data_map and "periods" in obj:
+            try:
+                # grab the periods from gungun col
+                g_periods = [
+                    obj["periods"]["periods"][1]["period"],
+                    obj["periods"]["periods"][3]["period"],
+                    obj["periods"]["periods"][5]["period"]
+                ]
+                
+                # now we append the gungundata and the current data
+                plot_ssnr.append(ssnr)
+                
+                plot_g1.append(g_periods[0])
+                plot_c1.append(current_data_map[ssnr][0])
+                
+                plot_g2.append(g_periods[1])
+                plot_c2.append(current_data_map[ssnr][1])
+                
+                plot_g3.append(g_periods[2])
+                plot_c3.append(current_data_map[ssnr][2])
+            except (IndexError, KeyError):
+                continue
+            
+    # "accurate" calculation
+      # first make them np arrays
+    plot_g1 = np.array(plot_g1)
+    plot_c1 = np.array(plot_c1)
+    plot_g2 = np.array(plot_g2)
+    plot_c2 = np.array(plot_c2)
+    plot_g3 = np.array(plot_g3)
+    plot_c3 = np.array(plot_c3)
+      # then we can do the math
+    accuracy1 = (plot_g1 == plot_c1).sum() / len(plot_g1)
+    accuracy2 = (plot_g2 == plot_c2).sum() / len(plot_g2)
+    accuracy3 = (plot_g3 == plot_c3).sum() / len(plot_g3)
+      # printing overall accuracy ( kinda lame )
+    print(f"how accurate .12-2hr: {accuracy1}")
+    print(f"Accuracy for period 2: {accuracy2}")
+    print(f"Accuracy for period 3: {accuracy3}")
 
-
-    # saves object data for gungun
-    async for object in gungunObjects:
-      ssnr = object["ssnamenr"]
-      per1 = object["periods"]["periods"][1]["period"]
-      per2 = object["periods"]["periods"][3]["period"]
-      per3 = object["periods"]["periods"][5]["period"]
-
-      gungunssnr.append(ssnr)
-      gungun1.append(per1)
-      gungun2.append(per2)
-      gungun3.append(per3)
+      # how many are on the line tho?
+    on_line1 = np.isclose(plot_g1, plot_c1, rtol=0.1).sum() / len(plot_g1)
+    on_line2 = np.isclose(plot_g2, plot_c2, rtol=0.1).sum() / len(plot_g2)
+    on_line3 = np.isclose(plot_g3, plot_c3, rtol=0.1).sum() / len(plot_g3)
+      # printing how many are on the line ( more interesting )
+    print(f"On line for period 1: {on_line1}")
+    print(f"On line for period 2: {on_line2}")
+    print(f"On line for period 3: {on_line3}")
 
     # plotting stuff
-    fig, axs = pyplot.subplots(4)
-    fig.suptitle('name v name & period v period')
-    axs[0].scatter(gungunssnr, currentssnr, label='ssnamenr')
-    axs[1].scatter(gungun1, current1, label='masked light periods .12-2 hr')
-    axs[2].scatter(gungun2, current2, label='masked light periods 2-100 hr')
-    axs[3].scatter(gungun3, current3, label='masked light periods 2.1-5000 hr')
-    for ax in axs.flat:
-      ax.set(xlabel='gungun', ylabel='daniel')
-      ax.legend()
+    fig, axs = pyplot.subplots(4, figsize=(10, 15))
+    fig.tight_layout(pad=5.0)
+    
+    # plot 1: make sure we actually do have the correct ids
+    axs[0].scatter(plot_ssnr, plot_ssnr, alpha=0.5)
+    axs[0].set_title("ssnr check")
+
+    # # plot 2: 
+    axs[1].scatter(plot_g1, plot_c1, color='blue', alpha=0.5)
+    axs[1].set_title(".12 - 2 hr period")
+    
+    # Plot 3: Period 2 Comparison
+    axs[2].scatter(plot_g2, plot_c2, color='green', alpha=0.5)
+    axs[2].set_title("2 - 100 hr period")
+
+    # Plot 4: Period 3 Comparison
+    # axs[3].scatter(plot_g3, plot_c3, color='red', alpha=0.5)
+    # axs[3].set_title("2.1 - 5000 hr period")
+
+    for ax in axs:
+        ax.set(xlabel='Gungun Data', ylabel='Daniel Data')
+        # Add a y=x line to see deviation
+        lims = [np.min([ax.get_xlim(), ax.get_ylim()]), np.max([ax.get_xlim(), ax.get_ylim()])]
+        ax.plot(lims, lims, 'k--', alpha=0.75, zorder=0)
 
     pyplot.show()
-
-
-    # # first clean the queried data
-    # async for gObj in gungunObjects:
-    #   async for cObj in currentObjects:
-    #     # if ssnamenr exists in other array, append
-    #     if (cObj["ssnamenr"] == gObj["ssnamenr"]):
-    #       print('in if statement')
-    #       gper1 = gObj["periods"]["periods"][1]["period"]
-    #       gper2 = gObj["periods"]["periods"][3]["period"]
-    #       gper3 = gObj["periods"]["periods"][5]["period"]
-    #       cper1 = cObj["periods"]["periods"][1]["period"]
-    #       cper2 = cObj["periods"]["periods"][3]["period"]
-    #       cper3 = cObj["periods"]["periods"][5]["period"]
-    #       ssnamenr = gObj["ssnamenr"]
-
-    #       gungunObjData1[0].append(ssnamenr)
-    #       gungunObjData2[0].append(ssnamenr)
-    #       gungunObjData3[0].append(ssnamenr)
-    #       gungunObjData1[1].append(gper1)
-    #       gungunObjData2[1].append(gper2)
-    #       gungunObjData3[1].append(gper3)
-
-    #       currentObjData1[0].append(ssnamenr)
-    #       currentObjData2[0].append(ssnamenr)
-    #       currentObjData3[0].append(ssnamenr)
-    #       currentObjData1[1].append(cper1)
-    #       currentObjData2[1].append(cper2)
-    #       currentObjData3[1].append(cper3)
-
-    # async for object in gungunObjects: # loops through all objects in what was found
-    #   if (object["periods"] != 0):
-    #     per1 = object["periods"]["periods"][1]["period"]
-    #     per2 = object["periods"]["periods"][3]["period"]
-    #     per3 = object["periods"]["periods"][5]["period"]
-    #     ssnamenr = object["ssnamenr"]
-    #     gungunObjData1[0].append(ssnamenr)
-    #     gungunObjData2[0].append(ssnamenr)
-    #     gungunObjData3[0].append(ssnamenr)
-    #     gungunObjData1[1].append(per1)
-    #     gungunObjData2[1].append(per2)
-    #     gungunObjData3[1].append(per3)
-    
-    # async for object in currentObjects:
-    #   if (object["periods"] != 0):
-    #     per1 = object["periods"]["periods"][1]["period"]
-    #     per2 = object["periods"]["periods"][3]["period"]
-    #     per3 = object["periods"]["periods"][5]["period"]
-    #     ssnamenr = object["ssnamenr"]
-    #     currentObjData1[0].append(ssnamenr)
-    #     currentObjData2[0].append(ssnamenr)
-    #     currentObjData3[0].append(ssnamenr)
-    #     currentObjData1[1].append(per1)
-    #     currentObjData2[1].append(per2)
-    #     currentObjData3[1].append(per3)
-
-    # for gName in gungunObjData1[0]:
-    #   for cName in currentObjData1[0]:
-    #     if (gName )
-    
-    # sort arrays (should sort both of them based on ssnamenr?)
-    # gungunObjData1[0], gungunObjData1[1] = zip(*sorted(zip(gungunObjData1[0], gungunObjData1[1])))
-    # currentObjData1[0], currentObjData1[1] = zip(*sorted(zip(currentObjData1[0], currentObjData1[1])))
-
-
-    # makes some stuff for plotting
-    # x1 = np.arange(0,len(gungunObjData),1)
-    # x2 = np.arange(0,len(currentObjData),1)
-    # y1 = np.array(gungunObjData)
-    # y2 = np.array(currentObjData)
-
-    # print(x1)
-
-    # fig, axs = pyplot.subplots(3)
-    # fig.suptitle('period')
-    # axs[0].scatter(gungunObjData1[0], gungunObjData1[1], label='gungun')
-    # axs[0].scatter(currentObjData1[0], currentObjData1[1], label='daniel')
-    # axs[1].scatter(gungunObjData2[0], gungunObjData2[1], label='gungun')
-    # axs[1].scatter(currentObjData2[0], currentObjData2[1], label='daniel')
-    # axs[2].scatter(gungunObjData3[0], gungunObjData3[1], label='gungun')
-    # axs[2].scatter(currentObjData3[0], currentObjData3[1], label='daniel')
-    # for ax in axs.flat:
-    #   ax.set(xlabel='gungun', ylabel='daniel')
-    #   ax.legend()
-
-    # pyplot.show()
-
-
-    # pyplot.scatter(gungunObjData1[0], gungunObjData1[1])
-    # pyplot.scatter(currentObjData1[0], currentObjData1[1])
-    # pyplot.xlabel('ssnamenr')
-    # pyplot.ylabel('period')
-    # pyplot.show()
     
     total_time = (time.time() - start_time)
+
+    # TODO:
+    # do a little math 
+    # use array1/array2 where > spec
+    # divide by whole len
 
 
     print("Closing connection...") # tells us
